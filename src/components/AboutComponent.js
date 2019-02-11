1e7fc2e0abac9d37fd27386ad866f3e4d8d1fcaa
James-W-Berry <james.berry.iv@gmail.com>
2019-02-11 18:25:55 -0500

    fetching leaders from server, posting feedback to server, animations for About Us leaders

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
 function About(props) {
 
-    const leaders = props.leaders.map((leader) => {
+    const leaders = props.leaders.leaders.map((leader) => {
         return (
             <RenderLeader leader={leader} />
         );
@@ -15,7 +17,7 @@ function About(props) {
             <div key={leader.id} className="col-12 mt-2">
                 <Media tag="li">
                     <Media left top>
-                        <Media object src={leader.image} alt={leader.name} />
+                        <Media object src={baseUrl + leader.image} alt={leader.name} />
                     </Media>
                     <Media body className="ml-5">
                         <Media heading>{leader.name}</Media>
@@ -82,9 +84,13 @@ function About(props) {
                     <h2>Corporate Leadership</h2>
                 </div>
                 <div className="col-12">
-                    <Media list>
-                        {leaders}
-                    </Media>
+                    <Stagger in>
+                        <Media list>
+                            <Fade in>
+                                {leaders}
+                            </Fade>
+                        </Media>
+                    </Stagger>
                 </div>
             </div>
         </div>
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -49,10 +49,15 @@ function Home(props) {
                     <RenderCard 
                         item={props.promotion} 
                         isLoading={props.promosLoading}
-                        errMess={props.promosErrMess} />
+                        errMess={props.promosErrMess} 
+                    />
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.leader} />
+                    <RenderCard
+                        item={props.leader} 
+                        isLoading={props.leadersLoading}
+                        errMess={props.leadersErrMess} 
+                    />
                 </div>
             </div>
         </div>
